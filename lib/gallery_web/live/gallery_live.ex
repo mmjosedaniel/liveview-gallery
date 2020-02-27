a4b29e0035aed12d456b0fc700888d7de0ece934
mmjosedaniel <daniel.martinez@kommit.co>
2020-02-27 16:22:21 -0500

    Add: Thumbnails

--- lib/gallery_web/live/gallery_live.ex
+++ lib/gallery_web/live/gallery_live.ex
@@ -7,6 +7,10 @@ defmodule GalleryWeb.GalleryLive do
 
   def render(assigns) do
     ~L"""
+    <%= for id <- Gallery.image_ids() do %>
+      <img src="<%= Gallery.thumb_url(id) %>"
+      class="<%= thumb_css_class(id, @current_id) %>">
+    <% end %>
     <label>Image id: <%= @current_id %></label>
     <button phx-click="prev">Prev</button>
     <button phx-click="next">Next</button>
@@ -31,5 +35,13 @@ defmodule GalleryWeb.GalleryLive do
   def assign_next_id(socket) do
     assign(socket, :current_id,
       Gallery.next_image_id(socket.assigns.current_id))
+  end
+
+  defp thumb_css_class(thumb_id, current_id) do
+    if thumb_id == current_id do
+      "thumb-selected"
+    else
+      "thumb-unselected"
+    end
   end
 end
