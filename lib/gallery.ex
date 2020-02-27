9e791e74cd13415e89504508094411b82d8d8ffb
mmjosedaniel <daniel.martinez@kommit.co>
2020-02-27 16:00:00 -0500

    Add: gallery base

--- lib/gallery.ex
+++ lib/gallery.ex
@@ -6,4 +6,28 @@ defmodule Gallery do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @unsplash_url "https://images.unsplash.com"
+
+  @ids [
+    "photo-1562971179-4ad6903a7ed6",
+    "photo-1552673597-e3cd6747a996",
+    "photo-1561133036-61a7ed56b424",
+    "photo-1530717449302-271006cdc1bf"
+  ]
+
+  def image_ids, do: @ids
+
+  def image_url(image_id, params) do
+    URI.parse(@unsplash_url)
+    |> URI.merge(image_id)
+    |> Map.put(:query, URI.encode_query(params))
+    |> URI.to_string()
+  end
+
+  def thumb_url(id),
+    do: image_url(id, %{w: 100, h: 100, fit: "crop"})
+
+  def large_url(id),
+    do: image_url(id, %{h: 500, fit: "crop"})
 end
--- lib/gallery_web/live/gallery_live.ex
+++ lib/gallery_web/live/gallery_live.ex
@@ -1,13 +1,49 @@
 defmodule GalleryWeb.GalleryLive do
   use Phoenix.LiveView
 
+  @images [
+    "https://images.unsplash.com/photo-1562971179-4ad6903a7ed6?h=500&fit=crop",
+    "https://images.unsplash.com/photo-1552673597-e3cd6747a996?h=500&fit=crop",
+    "https://images.unsplash.com/photo-1561133036-61a7ed56b424?h=500&fit=crop",
+    "https://images.unsplash.com/photo-1530717449302-271006cdc1bf?h=500&fit=crop"
+  ]
+
+  @images_count Enum.count(@images)
+
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :idx, 0)}
   end
 
   def render(assigns) do
     ~L"""
-    <h1>LiveView is awesome!</h1>
+    <label>Image Index: <%= @idx %></label>
+    <button phx-click="prev">Prev</button>
+    <button phx-click="next">Next</button>
+
+    <img src="<%= image(@idx) %>">
     """
+  end
+
+  def handle_event("prev", _event, socket) do
+    {:noreply, assign_prev_idx(socket)}
+  end
+
+  def handle_event("next", _event, socket) do
+    {:noreply, assign_next_idx(socket)}
+  end
+
+  def assign_prev_idx(socket) do
+    socket
+    |> update(:idx, &(&1 - 1))
+  end
+
+  def assign_next_idx(socket) do
+    socket
+    |> update(:idx, &(&1 + 1))
+  end
+
+  def image(idx) do
+    idx = rem(idx, @images_count)
+    Enum.at(@images, idx)
   end
 end
